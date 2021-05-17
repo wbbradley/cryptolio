99dd0cac5f22a80aa17dcf42dd5115573de13345
William Bradley <williambbradley@gmail.com>
2021-05-16 19:57:14 -0600

    ic

---
+++ go.mod
@@ -0,0 +1,8 @@
+module cryptolio
+
+go 1.16
+
+require (
+	github.com/anaskhan96/soup v1.2.4 // indirect
+	github.com/coincircle/go-coinmarketcap v0.0.0-20180522222104-86a427de333f
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	cmc "github.com/coincircle/go-coinmarketcap"
+)
+
+func main() {
+	tickers, err := cmc.Tickers(&cmc.TickersOptions{
+		Start:   0,
+		Limit:   100,
+		Convert: "USD",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ticker := range tickers {
+		fmt.Println(ticker.Symbol, ticker.Quotes["USD"].Price)
+	}
+}
